639fa71bb2537afd8dd5c0a6935c4684cd660169
Lakshmi <Lakshmi@NTPLAP-008.chn.nestgroup.net>
2022-10-20 17:31:52 +0530

    push

---
+++ src/app/searchflight/searchflight.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-searchflight',
+  templateUrl: './searchflight.component.html',
+  styleUrls: ['./searchflight.component.css']
+})
+export class SearchflightComponent implements OnInit {
+
+  constructor(private myapi:ApiService) { }
+
+  origin=""
+
+  searchFlight=()=>{
+    let data={
+      "origin":this.origin
+    }
+    console.log(data)
+    this.myapi.searchFlight(data).subscribe(
+      (res)=>{
+        this.data=res
+      }
+    )
+  }
+
+  data:any=[]
+  ngOnInit(): void {
+  }
+
+}
